4f0e7a28b7a31f0a2c68ed1b73c20a95c155f77f
mattb <mattb.frye@gmail.com>
2022-04-05 10:09:02 -0400

    commit on master

--- main.py
+++ main.py
@@ -9,37 +9,36 @@ root = tk.Tk()
 # root window title and dimension
 root.title("mattFrye Pxlart")
 # Set geometry (widthxheight)
-root.geometry('350x200')
+root.geometry('640x360')
 
-img = tk.PhotoImage(file='Sprite-0001.gif', format= "gif -index 2")
+img1 = tk.PhotoImage(file='Sprite-0001.gif')
 
-label = Label(root, image = img).grid(row = 0, column = 2,
-       columnspan = 10, rowspan = 10, padx = 5, pady = 5)
+img2 = tk.PhotoImage(file='Sprite-0002.png')
+
+label = Label(root, image = img1)
+label.grid(column=2,row=0)
+   
 
 
 
 
-def clicked():
+def clicked1():
     
-    label = 1
+    label = Label(root,image = img1)
+
+def clicked2():
+    
+    label = Label(root, image = img2)
     
     
  
-btn1 = Button(root, text = "Eyeball " ,fg = "purple",command=clicked)
-btn2 = Button(root, text = "Click me " ,fg = "red",command=clicked)
-btn3 = Button(root, text = "Click me " ,fg = "red",command=clicked)
-btn4 = Button(root, text = "Click me " ,fg = "red",command=clicked)
-
-
-
-
-
+btn1 = Button(root, text = "Eyeball " ,fg = "purple",command=clicked1)
+btn2 = Button(root, text = "Landscape " ,fg = "green",command=clicked2)
 
 
 btn1.grid(column=1,row=0)
 btn2.grid(column=1,row=1)
-btn3.grid(column=1,row=2)
-btn4.grid(column=1,row=3)
+
 
 
 
